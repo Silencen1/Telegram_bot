37f7b7bc0d5bae198814d6aac61ab501b146165e
Abdulaziz <Uzstarsxtv@gmail.com>
2025-05-15 20:15:59 +0500

    Logolar menyusiga ma'lumotlar qo'shildi

--- main.py
+++ main.py
@@ -46,74 +46,40 @@ MEDIA_MAP = {
         'https://kitdls.net/wp-content/uploads/2023/12/DLS-512x512-Logo-Manchester-City.png',
         'https://kitdls.net/wp-content/uploads/2024/01/dls-512x512-logo-al-nassr.png',
         'https://kitdls.net/wp-content/uploads/2023/09/Logo-Barcelona-Dream-League-Soccer-1.png',
-        'https://kitdls.net/wp-content/uploads/2023/09/Logo-Barcelona-Dream-League-Soccer-2.png',
         'https://kitdls.net/wp-content/uploads/2023/09/Logo-Manchester-Utd-2023-2024-DLS-1.png',
-        'https://kitdls.net/wp-content/uploads/2023/09/Logo-Manchester-Utd-2023-2024-DLS.png'
+        'https://i.ibb.co/9sK4gJB/logo.png',
+        'https://i.imgur.com/cqqOaVI.png',
+        'https://i.imgur.com/TTlk1j7.png',
+        'https://i.imgur.com/MyyAgmt.png',
+        'https://i.imgur.com/HtOztrQ.png',
+        'https://i.imgur.com/avU3cY9.png',
+        ''
+
     ],
     'Manchestr City': [
         'https://dlsgame.net/wp-content/uploads/2025/03/MC-Home-Kit-25-DLS-25.png',
         'https://dlsgame.net/wp-content/uploads/2025/03/MC-Away-Kit-25-DLS-25.png',
         'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-Home-Kit-V2-2024-2025-DLS-24.png',
         'https://kitdls.net/wp-content/uploads/2024/09/Manchester-City-Kit-X-Kevin-De-Bruyne-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-Away-Kit-V2-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/09/Manchester-City-Kit-X-Kevin-De-Bruyne-2-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/DLS-24-Manchester-City-Third-Kit-V2-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Manchester-City-Four-Kits-2025-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MC-GK-Kit-1-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MC-GK-Kit-2-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-GK-Home-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-GK-Away-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-GK-Third-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/DLS-24-Manchester-City-Dragon-Kit-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-manchester-city-home-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-manchester-city-away-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-manchester-city-third-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-manchester-city-gk1-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-manchester-city-gk3-2023-2024-dls-2024.png'
+        'https://kitdls.net/wp-content/uploads/2024/08/Manchester-City-Away-Kit-V2-2024-2025-DLS-24.png'
+
     ],
     'Al-Nasr': [
         'https://dlsgame.net/wp-content/uploads/2025/03/Al-Nassr-Home-Kit-25-DLS-25.png',
         'https://dlsgame.net/wp-content/uploads/2025/03/Al-Nassr-Away-Kit-25-DLS-25.png',
         'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-Home-Kit-V2-2024-2025-DLS-24.png',
         'https://kitdls.net/wp-content/uploads/2024/08/Al-Nassr-Home-Kit-X-CR7.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-Away-Kit-V2-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Al-Nassr-Away-Kit-X-CR7.png',
-        'https://kitdls.net/wp-content/uploads/2024/03/Al-Nassr-Third-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-GK-Home-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-GK-Away-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-GK-Third-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-AL-Nassr-Home-2023-2024-DLS-24-1.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-AL-Nassr-Home-2023-2024-DLS-24-2.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-AL-Nassr-Away-2023-2024-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-AL-Nassr-GK-2023-2024-DLS-24.png'
+        'https://kitdls.net/wp-content/uploads/2024/05/Al-Nassr-Away-Kit-V2-2024-2025-DLS-24.png'
+
     ],
     'Real Madrid': [
         'https://i.ibb.co/h8Z578f/image.png',
         'https://i.ibb.co/kg8W9YN/image.png',
         'https://kitdls.net/wp-content/uploads/2024/05/DLS-24-Real-Madrid-Home-Kit-V3-2024-2025.png',
         'https://kitdls.net/wp-content/uploads/2024/05/Kit-Real-Madrid-2024-2025-CR7-1.png',
-        'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-Real-Madrid-Third-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/DLS-24-Real-Madrid-Third-Kit-V2-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/09/DLS-24-Real-Madrid-GK-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/10/DLS-24-Real-Madrid-GK-Kit-2-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/10/DLS-24-Real-Madrid-GK-Kit-3-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-home-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-away-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-third-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-gk1-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-gk2-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-real-madrid-gk3-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Home-Kit-2009-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Away-Kit-2009-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Third-Kit-2009-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Home-Kit-2007-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Away-Kit-2007-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Third-Kit-2007-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Home-Kit-2004-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Third-Kit-2004-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Home-Kit-2000-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Away-Kit-2000-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/Real-Madrid-Third-Kit-2000-DLS-25.png'
+        'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-Real-Madrid-Third-Kit-2024-2025.png'
+
+
     ],
     'Barcelona': [
         'https://dlsgame.net/wp-content/uploads/2025/04/Barcelona-Home-Kit-2005-06-DLS-25.png',
@@ -121,20 +87,7 @@ MEDIA_MAP = {
         'https://kitdls.net/wp-content/uploads/2024/05/DLS-24-Barcelona-Home-Kit-V2-2024-2025.png',
         'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-Home-Kit-2024-2025-DLS-24.png',
         'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-Away-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-Third-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-Four-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-GK-Home-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/07/Barcelona-GK-Away-Kit-2024-2025-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2025/05/Barcelone-Home-Kit-X-Travis-Scott-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/05/Barcelone-Away-Kit-X-Travis-Scott-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-home-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-away-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-third-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-gk1-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-gk2-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/kit-barcelona-gk3-2023-2024-dls-2024.png',
-        'https://kitdls.net/wp-content/uploads/2024/12/Barcelone-Home-Kit-2000-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2024/12/Barcelone-Away-Kit-2000-DLS-25.png',
+
         ],
      'Manchestr United': [
         # 1)
@@ -144,27 +97,8 @@ MEDIA_MAP = {
         # 3)
         'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-MU-Home-Kit-2024-2025.png',
         'https://kitdls.net/wp-content/uploads/2024/08/Kit-MU-2024-CR7.png',
-        'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-MU-Away-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/Kit-MU-2024-CR7-2.png',
-        'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-MU-Third-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/08/DLS-24-MU-Third-Kit-V2-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/06/DLS-24-MU-GK-Home-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2024/06/DLS-24-MU-GK-Away-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/DLS-24-MU-GK-Third-Kit-2024-2025.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-Home-2023-2024-DLS-2024-0.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-Away-2023-2024-DLS-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-Third-2023-2024-DLS-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-GK1-2023-2024-DLS-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-GK2-2023-2024-DLS-2024.png',
-        'https://kitdls.net/wp-content/uploads/2023/12/Kit-Manchester-United-GK3-2023-2024-DLS-2024.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/MU-Home-Kit-2007-2008-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/MU-Home-Kit-2-2007-2008-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/MU-Away-Kit-2007-2008-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2024/05/MU-GK-Kit-2007-2008-DLS-24.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MU-Vodafone-1-Kit-25-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MU-Vodafone-2-Kit-25-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MU-Vodafone-3-Kit-25-DLS-25.png',
-        'https://kitdls.net/wp-content/uploads/2025/02/MU-Vodafone-4-Kit-25-DLS-25.png'
+        'https://kitdls.net/wp-content/uploads/2024/02/DLS-24-MU-Away-Kit-2024-2025.png'
+
     ],
 }
 
