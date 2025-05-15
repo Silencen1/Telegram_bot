251772dcfc0f2b57c6b54fb13eaba3b104d77605
Abdulaziz <Uzstarsxtv@gmail.com>
2025-05-15 19:52:58 +0500

    BOT_TOKEN environment variable orqali o'qiladigan qilib o'zgartirildi

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import os
 from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
 # Log sozlamalari
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
-token = os.getenv("TOKEN")
+
 # Fayldan o'qish/yozish funksiyalari
 VERIFIED_FILE = 'verified_users.json'
 
